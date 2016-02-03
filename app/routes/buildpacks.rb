861ac6bb92759e2db07f62ecdb0f3a31775f4d9e
Stev Witzel <switzel@pivotal.io>
2016-02-03 12:50:34 +0000

    Store uploaded buildpack in blobstore
    This uses most of the existing blobstore code. For now the config is
    hard-coded within the route. We will move that out in a future commit.
    
    Added additional unit tests to verify the interaction inside the route.
    
    [#112526973]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- app/routes/buildpacks.rb
+++ app/routes/buildpacks.rb
@@ -2,6 +2,30 @@ module Bits
   module Routes
     class Buildpacks < Sinatra::Application
       put '/buildpacks/:guid' do
+        config = {
+          buildpacks: {
+            fog_connection: {
+              provider: 'AWS',
+              aws_access_key_id: 'fake_aws_key_id',
+              aws_secret_access_key: 'fake_secret_access_key',
+            },
+          },
+          nginx: {
+            use_nginx: false,
+          },
+        }
+
+        blobstore = BlobstoreFactory.new(config).create_buildpack_blobstore
+
+        upload_params = UploadParams.new(params, use_nginx: config[:nginx][:use_nginx])
+
+        source_path = upload_params.upload_filepath('buildpack')
+
+        sha = Digester.new.digest_path(source_path)
+        destination_key = "#{params[:guid]}_#{sha}"
+
+        blobstore.cp_to_blobstore(source_path, destination_key)
+
         status 201
       end
     end
