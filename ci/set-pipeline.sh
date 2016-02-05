4559a8807dd3a78e831abcf78dec834eb590ca9c
Tiago Scolari <tscolari@pivotal.io>
2016-02-05 14:32:14 +0000

    Include feature flag ON/OFF deployments to pipeline
    CATs should now be running agains deployments with and without the flag
    on.
    
    [#112586915]

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
-fly -t flintstone set-pipeline -p bits-service -c pipeline.yml -l config.yml
+fly -t flintstone set-pipeline -p bits-service-wip -c pipeline.yml -l config.yml
