d771483bb88fecd83066611d5d7796c678daef62
Tiago Scolari <tscolari@pivotal.io>
2016-02-05 15:10:38 +0000

    Add GitHub private-key to the set-pipeline script
    [#112586915]

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -4,4 +4,5 @@ cd $(dirname $0)
 
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
-fly -t flintstone set-pipeline -p bits-service-wip -c pipeline.yml -l config.yml
+ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
+fly -t flintstone set-pipeline -p bits-service-wip -c pipeline.yml -l config.yml -v github-private-key="${ssh_key}"
