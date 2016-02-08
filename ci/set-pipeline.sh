66dbb4df36ca3c951dd57a5b2660df32247dc835
Tiago Scolari <tscolari@pivotal.io>
2016-02-08 11:14:34 +0000

    Reset the `set-pipeline` script to the main pipeline

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
 ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
-fly -t flintstone set-pipeline -p bits-service-wip -c pipeline.yml -l config.yml -v github-private-key="${ssh_key}"
+fly -t flintstone set-pipeline -p bits-service -c pipeline.yml -l config.yml -v github-private-key="${ssh_key}"
