22b9ae086fcfc509576fc4aa1d69cea554d272cb
Pivotal <pivotal@aramexci.lon.pivotallabs.com>
2016-02-26 12:14:39 +0000

    WIP

--- lib/bits_service/routes/app_stash.rb
+++ lib/bits_service/routes/app_stash.rb
@@ -24,23 +24,52 @@ module BitsService
       end
 
       post '/app_stash/matches' do
-        begin
-          request.body.rewind
-          request_payload = JSON.parse request.body.read
-        rescue JSON::ParserError => e
-          fail Errors::ApiError.new_from_details('MessageParseError', e.message)
-        end
-
-        unless request_payload.is_a?(Array)
-          fail Errors::ApiError.new_from_details('UnprocessableEntity', 'must be an array.')
-        end
-
         response_payload = request_payload.select do |resource|
           resource.key?('sha1') && app_stash_blobstore.exists?(resource['sha1'])
         end
 
         json 200, response_payload
       end
+
+      post '/app_stash/bundles' do
+        destination_dir = Dir.mktmpdir('app_cache')
+
+        unless request_payload.all? { |e| e['fn'] && e['sha1'] }
+          fail Errors::ApiError.new_from_details('UnprocessableEntity', 'must specify sha1 and fn for each entry')
+        end
+
+        unless request_payload.all? { |e| app_stash_blobstore.exists? e['sha1']  }
+          fail Errors::ApiError.new_from_details('NotFound', 'requested sha1 not found in blob store')
+        end
+
+        request_payload.each do |file_spec|
+          destination_path = File.join(destination_dir, file_spec['fn'])
+          app_stash_blobstore.download_from_blobstore(file_spec['sha1'], destination_path)
+        end
+
+        zip_dir = Dir.mktmpdir('app_cache')
+        zip_path = File.join(zip_dir, 'package.zip')
+        SafeZipper.zip(destination_dir, zip_path)
+
+        status 200
+        send_file zip_path, type: :zip
+      end
+
+      private
+
+      def request_payload
+        return @request_payload if @request_payload
+        @request_payload = JSON.parse request.body.read
+
+        unless request_payload.is_a?(Array)
+          fail Errors::ApiError.new_from_details('UnprocessableEntity', 'must be an array.')
+        end
+
+        @request_payload
+      rescue JSON::ParserError => e
+        fail Errors::ApiError.new_from_details('MessageParseError', e.message)
+      end
+
     end
   end
 end
--- spec/integration/app_stash_spec.rb
+++ spec/integration/app_stash_spec.rb
@@ -31,6 +31,8 @@ describe 'app_stash endpoint', type: :integration do
   end
 
   let(:zip_filepath) { File.expand_path('../../fixtures/integration/app.zip', __FILE__) }
+  let(:app_rb_sha) { '8b381f8864b572841a26266791c64ae97738a659' }
+  let(:lib_rb_sha) { '594eb15515c89bbfb0874aa4fd4128bee0a1d0b5' }
 
   describe 'POST /app_stash/entries' do
     let(:request_body) { { application: File.new(zip_filepath) } }
@@ -43,10 +45,10 @@ describe 'app_stash endpoint', type: :integration do
     it 'stores all the files from the zip based in their SHAs' do
       make_post_request('/app_stash/entries', request_body)
 
-      app_rb = blob_path(@root_dir, 'app_stash', '8b381f8864b572841a26266791c64ae97738a659')
+      app_rb = blob_path(@root_dir, 'app_stash', app_rb_sha)
       expect(File.exist?(app_rb)).to eq(true)
 
-      lib_rb = blob_path(@root_dir, 'app_stash', '594eb15515c89bbfb0874aa4fd4128bee0a1d0b5')
+      lib_rb = blob_path(@root_dir, 'app_stash', lib_rb_sha)
       expect(File.exist?(lib_rb)).to eq(true)
     end
 
@@ -85,7 +87,7 @@ describe 'app_stash endpoint', type: :integration do
     subject(:response) { make_post_request('/app_stash/matches', request_body, content_type: :json) }
 
     let(:existing_shas) do
-      ['8b381f8864b572841a26266791c64ae97738a659', '594eb15515c89bbfb0874aa4fd4128bee0a1d0b5']
+      [app_rb_sha, lib_rb_sha]
     end
 
     let(:non_existing_shas) do
@@ -190,6 +192,151 @@ describe 'app_stash endpoint', type: :integration do
         payload = JSON.parse(response.body)
         expect(payload).to be_a(Array)
         expect(payload).to be_empty
+      end
+    end
+  end
+
+  describe 'POST /app_stash/bundles' do
+    context 'with temp download folder' do
+      before :each do
+        @download_dir = Dir.mktmpdir
+      end
+
+      after(:each) do
+        FileUtils.rm_rf(@download_dir)
+      end
+
+      before do
+        zip_filepath = File.expand_path('../../fixtures/integration/app.zip', __FILE__)
+        request_body = { application: File.new(zip_filepath)  }
+        make_post_request('/app_stash/entries', request_body)
+      end
+
+      subject(:response) { make_post_request('/app_stash/bundles', request_body, content_type: :json) }
+      let(:request_body) { request_payload.to_json }
+
+      def write_response_body_to_file(response)
+        download_zip_path = "#{@download_dir}/result.zip"
+        File.open(download_zip_path, 'w') do |w|
+          w.write(response.body)
+        end
+        download_zip_path
+      end
+
+      shared_examples 'bundles endpoint' do
+        it 'returns HTTP status 200' do
+          expect(response.code).to eq(200)
+        end
+
+        it 'returns an application package' do
+          download_zip_path = write_response_body_to_file(response)
+
+          FileUtils.mkdir_p (unzip_path = "#{@download_dir}/unzip")
+
+          BitsService::SafeZipper.unzip!(download_zip_path, unzip_path)
+
+          unzipped_files = Dir[File.join(unzip_path, '**', '*')].reject{ |f| File.directory?(f) }.sort
+          expected_files = request_payload.map { |spec| File.join(unzip_path, spec[:fn])}.sort
+          expect(unzipped_files).to eq(expected_files)
+
+          request_payload.each do |spec|
+            unzipped_file_path = File.join(unzip_path, spec[:fn])
+            expect(BitsService::Digester.new.digest_path(unzipped_file_path)).to eq(spec[:sha1])
+          end
+        end
+      end
+
+      context 'a single file' do
+        let(:request_payload) do
+          [
+            { fn: "init.rb", sha1: app_rb_sha }
+          ]
+        end
+
+        it_behaves_like 'bundles endpoint'
+      end
+
+      context 'multiple files' do
+        let(:request_payload) do
+          [
+            { fn: "init.rb", sha1: app_rb_sha },
+            { fn: "lib.rb", sha1: lib_rb_sha },
+            { fn: "another_one.rb", sha1: lib_rb_sha }
+          ]
+        end
+
+        it_behaves_like 'bundles endpoint'
+      end
+
+      context 'folders and subfolders' do
+        let(:request_payload) do
+          [
+            { fn: "init.rb", sha1: app_rb_sha },
+            { fn: "some-folder/lib.rb", sha1: lib_rb_sha },
+            { fn: "another-folder/inside/another_one.rb", sha1: lib_rb_sha },
+            { fn: "some-folder/subfolder/app.rb", sha1: app_rb_sha }
+          ]
+        end
+
+        it_behaves_like 'bundles endpoint'
+      end
+
+      context 'when the blobstore does not have an entry' do
+        let(:request_payload) do
+          [
+            { fn: "init.rb", sha1: app_rb_sha },
+            { fn: "lib.rb", sha1: 'i-do-not-exist' },
+            { fn: "another_one.rb", sha1: lib_rb_sha }
+          ]
+        end
+
+        it 'returns HTTP status 404' do
+          expect(response.code).to eq(404)
+        end
+
+        it 'returns a corresponding error' do
+          description = JSON.parse(response.body)['description']
+          expect(description).to match(/Unknown request/)
+        end
+      end
+
+      context 'when the body is empty' do
+        let(:request_body) { nil }
+
+        it 'returns HTTP status 400' do
+          expect(response.code).to eq(400)
+        end
+
+        it 'returns a corresponding error' do
+          description = JSON.parse(response.body)['description']
+          expect(description).to match(/Request invalid due to parse error:/)
+        end
+      end
+
+      context 'when the body is invalid JSON' do
+        let(:request_body) { 'some-invalid-json' }
+
+        it 'returns HTTP status 400' do
+          expect(response.code).to eq(400)
+        end
+
+        it 'returns a corresponding error' do
+          description = JSON.parse(response.body)['description']
+          expect(description).to match(/Request invalid due to parse error:/)
+        end
+      end
+
+      context 'when the body is not a JSON list' do
+        let(:request_body) { { foo: 'bar' }.to_json }
+
+        it 'returns HTTP status 422' do
+          expect(response.code).to eq(422)
+        end
+
+        it 'returns a corresponding error' do
+          description = JSON.parse(response.body)['description']
+          expect(description).to match(/The request is semantically invalid:/)
+        end
       end
     end
   end
