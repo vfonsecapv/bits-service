13ef76f118e174656c9b45b5bfd53eb905fb7cc9
Stev Witzel <switzel@pivotal.io>
2016-02-26 12:24:15 +0000

    Add POST /app_stash/bundles
    [#114224821]
    
    Signed-off-by: Konstantinos Karampogias <karampok@gmail.com>
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- lib/bits_service/routes/app_stash.rb
+++ lib/bits_service/routes/app_stash.rb
@@ -10,7 +10,7 @@ module BitsService
           'AppBitsUploadInvalid',
           'missing key `application`') unless uploaded_filepath
 
-        destination_path = Dir.mktmpdir('app_cache')
+        destination_path = Dir.mktmpdir('app_stash')
         begin
           SafeZipper.unzip!(uploaded_filepath, destination_path)
           app_stash_blobstore.cp_r_to_blobstore(destination_path)
@@ -40,6 +40,51 @@ module BitsService
         end
 
         json 200, response_payload
+      end
+
+      post '/app_stash/bundles' do
+        begin
+          request.body.rewind
+          request_payload = JSON.parse request.body.read
+        rescue JSON::ParserError => e
+          fail Errors::ApiError.new_from_details('MessageParseError', e.message)
+        end
+
+        unless request_payload.is_a?(Array) && !request_payload.empty?
+          fail Errors::ApiError.new_from_details('UnprocessableEntity', 'must be a non-empty array.')
+        end
+
+        destination_path = Dir.mktmpdir('app_stash')
+
+        request_payload.each do |resource|
+          sha1 = resource['sha1']
+          unless sha1 && sha1.to_s != ''
+            fail Errors::ApiError.new_from_details('UnprocessableEntity', 'key `sha1` missing or empty')
+          end
+
+          fn = resource['fn']
+          unless fn && fn.to_s != ''
+            fail Errors::ApiError.new_from_details('UnprocessableEntity', 'key `fn` missing or empty')
+          end
+
+          unless app_stash_blobstore.exists?(sha1)
+            fail Errors::ApiError.new_from_details('ResourceNotFound', "#{sha1} not found")
+          end
+
+          app_stash_blobstore.download_from_blobstore(sha1, File.join(destination_path, fn))
+        end
+
+        output_path = File.join(Dir.mktmpdir('app_stash'), 'bundle.zip')
+        SafeZipper.zip(destination_path, output_path)
+        FileUtils.rm_rf(destination_path)
+
+        stream do |out|
+          begin
+            out << File.new(output_path).read
+          ensure
+            FileUtils.rm_r(output_path)
+          end
+        end
       end
     end
   end
--- spec/integration/app_stash_spec.rb
+++ spec/integration/app_stash_spec.rb
@@ -193,4 +193,149 @@ describe 'app_stash endpoint', type: :integration do
       end
     end
   end
+
+  describe 'POST /app_stash/bundles' do
+    subject(:response) { make_post_request('/app_stash/bundles', request_body) }
+
+    let(:existing_resources) do
+      [
+        { 'sha1' => '8b381f8864b572841a26266791c64ae97738a659', 'fn' => 'app/app.rb' },
+        { 'sha1' => '594eb15515c89bbfb0874aa4fd4128bee0a1d0b5', 'fn' => 'app/lib.rb' },
+      ]
+    end
+
+    let(:nonexisting_resources) do
+      [
+        { 'sha1' => 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'fn' => 'app/not-there' },
+      ]
+    end
+
+    let(:request_body) { existing_resources.to_json }
+
+    let(:zip_filepath) { File.expand_path('../../fixtures/integration/app.zip', __FILE__) }
+
+    let(:zip_file_sha1) { BitsService::Digester.new.digest_path(zip_filepath) }
+
+    before(:each) do
+      request_body = { application: File.new(zip_filepath) }
+      make_post_request('/app_stash/entries', request_body)
+    end
+
+    def count_files_in_dir(dir)
+      Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
+    end
+
+    it 'returns HTTP status 200' do
+      expect(response.code).to eq(200)
+    end
+
+    it 'returns the correct zip file' do
+      zip = Tempfile.new('zipfile')
+      zip.write(response.body)
+      zip.close
+
+      unzipped_path = Dir.mktmpdir('test')
+      BitsService::SafeZipper.unzip!(zip.path, unzipped_path)
+
+      existing_resources.each do |resource|
+        path = File.join(unzipped_path, resource['fn'])
+        expect(File).to exist(path)
+        sha1 = BitsService::Digester.new.digest_path(path)
+        expect(sha1).to eq(resource['sha1'])
+      end
+
+      expect(count_files_in_dir(unzipped_path)).to eq(existing_resources.size)
+    end
+
+    context 'when the request is for non-existing resources' do
+      let(:request_body) { (existing_resources + nonexisting_resources).to_json }
+
+      it 'returns HTTP status 404' do
+        expect(response.code).to eq(404)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to eq('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa not found')
+      end
+    end
+
+    context 'when the request body is empty' do
+      let(:request_body) { nil }
+
+      it 'returns HTTP status 400' do
+        expect(response.code).to eq(400)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/Request invalid due to parse error/)
+      end
+    end
+
+    context 'when the request body is invalid json' do
+      let(:request_body) { 'invalid' }
+
+      it 'returns HTTP status 400' do
+        expect(response.code).to eq(400)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/Request invalid due to parse error/)
+      end
+    end
+
+    context 'when the json in the request is not an array' do
+      let(:request_body) { '{}' }
+
+      it 'returns HTTP status 422' do
+        expect(response.code).to eq(422)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/The request is semantically invalid: must be a non-empty array/)
+      end
+    end
+
+    context 'when the json in the request is an empty array' do
+      let(:request_body) { '[]' }
+
+      it 'returns HTTP status 422' do
+        expect(response.code).to eq(422)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/The request is semantically invalid: must be a non-empty array/)
+      end
+    end
+
+    context 'when the json in the request contains a resource without a SHA' do
+      let(:request_body) { '[{"fn":"/app/lib.rb"}]' }
+
+      it 'returns HTTP status 422' do
+        expect(response.code).to eq(422)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/The request is semantically invalid: key `sha1` missing or empty/)
+      end
+    end
+
+    context 'when the json in the request contains a resource without a file path' do
+      let(:request_body) { '[{"sha1":"zyzfh"}]' }
+
+      it 'returns HTTP status 422' do
+        expect(response.code).to eq(422)
+      end
+
+      it 'returns a corresponding error' do
+        description = JSON.parse(response.body)['description']
+        expect(description).to match(/The request is semantically invalid: key `fn` missing or empty/)
+      end
+    end
+  end
 end
