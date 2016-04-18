049e68cc98e9465d1605cea458cf3a7ba2430cb4
Stev Witzel <switzel@pivotal.io>
2016-04-18 16:56:26 +0100

    Download stemcell to the bosh-lite host machine
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/recreate-bosh-lite.sh
+++ ci/scripts/recreate-bosh-lite.sh
@@ -38,7 +38,7 @@ function setup_boshlite {
 
 function upload_stemcell {
   echo "-- Uploading stemcell"
-  wget "https://s3.amazonaws.com/bosh-warden-stemcells/bosh-stemcell-3147-warden-boshlite-ubuntu-trusty-go_agent.tgz"
+  ssh $SSH_CONNECTION_STRING "wget 'https://s3.amazonaws.com/bosh-warden-stemcells/bosh-stemcell-3147-warden-boshlite-ubuntu-trusty-go_agent.tgz'"
   ssh $SSH_CONNECTION_STRING "bosh upload stemcell bosh-stemcell-3147-warden-boshlite-ubuntu-trusty-go_agent.tgz"
 }
 
