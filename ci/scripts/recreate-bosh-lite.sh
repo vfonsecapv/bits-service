1210724cddd2107462b49d6df16626cd2cd9ab30
Tiago Scolari <tscolari@pivotal.io>
2016-04-20 17:42:32 +0100

    Automatically upload diego related releases on recreate

--- ci/scripts/recreate-bosh-lite.sh
+++ ci/scripts/recreate-bosh-lite.sh
@@ -32,6 +32,9 @@ function setup_boshlite {
   echo "-- Logging in to the new director"
   ssh $SSH_CONNECTION_STRING "bosh target https://$BOSH_DIRECTOR_IP:25555 && bosh login admin admin"
   upload_stemcell
+  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=${DIEGO_RELEASE_VERSION}"
+  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=${ETCD_RELEASE_VERSION}"
+  upload_remote_release "https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=${GARDEN_LINUX_RELEASE_VERSION}"
   echo "-- Changing default user"
   ssh $SSH_CONNECTION_STRING "bosh create user $BOSH_USERNAME $BOSH_PASSWORD"
 }
@@ -42,12 +45,19 @@ function upload_stemcell {
   ssh $SSH_CONNECTION_STRING "bosh upload stemcell stemcell.tgz"
 }
 
-
 function set_networking {
   echo "-- Setting up networking"
   ssh $SSH_CONNECTION_STRING "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward"
   ssh $SSH_CONNECTION_STRING "ip route add 10.250.0.0/16 via $BOSH_DIRECTOR_IP"
   ssh $SSH_CONNECTION_STRING "cd ~/workspace/bosh-lite && vagrant ssh -- 'sudo ip route add 10.155.248.0/24 via $VAGRANT_GATEWAY dev eth1'"
+}
+
+function upload_remote_release {
+  local release_url=$1
+  echo "-- Uploading release: ${release_url}"
+
+  wget "$release_url" -O remote_release.tgz
+  ssh $SSH_CONNECTION_STRING "bosh upload release remote_release.tgz"
 }
 
 main
