ca3dde2d1a9ab259f6564e50383856e12a542f68
Tiago Scolari <tscolari@pivotal.io>
2016-04-20 12:52:53 +0200

    Update pipeline to upload files to acceptance
    [#117927603]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -4,5 +4,14 @@ cd $(dirname $0)
 
 # use brew install lastpass-cli
 lpass show "Shared-Flintstone"/ci-config --notes > config.yml
-ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
-fly -t flintstone set-pipeline -p bits-service -c pipeline.yml -l config.yml -v github-private-key="${ssh_key}"
+github_ssh_key=$(lpass show "Shared-Flintstone"/Github --notes)
+acceptance_ssh_key=$(lpass show "Shared-Flintstone"/Acceptance --notes)
+
+fly \
+  set-pipeline \
+  -t flintstone \
+  -p bits-service \
+  -c pipeline.yml \
+  -l config.yml \
+  -v github-private-key="${github_ssh_key}" \
+  -v acceptance-private-key="${acceptance_ssh_key}"
