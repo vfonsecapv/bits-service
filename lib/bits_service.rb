9ef22cc9174bf0f2dbb277450f901c2fd48cb734
Peter Goetz <peter.gtz@gmail.com>
2016-06-01 14:36:32 +0200

    Sort required files to avoid inconsistencies
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- lib/bits_service.rb
+++ lib/bits_service.rb
@@ -1,3 +1,4 @@
-Dir[File.expand_path('../**/*.rb', __FILE__)].each do |file|
-  require file
-end
+# Note that this kind of requiring all files can potentially cause issues when
+# files have dependencies on others that conflict with the ordering of this listing
+# We might want to unroll this loop to avoid this issue.
+Dir[File.expand_path('../**/*.rb', __FILE__)].sort.each { |f| require f }
